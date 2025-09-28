80bbb69ee462e47b82ef499da811a9d663289853
2002jaebin-rgb <2002jaebin@gmail.com>
2025-09-28 20:43:57 +0900

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Navbar from './Navbar'
 
 /**
- * 공통 레이아웃
- * - 상단 Navbar
- * - 중앙 컨테이너
- * - 모바일/PWA 환경 고려한 padding
+ * Layout 컴포넌트
+ * - Navbar 항상 상단에 표시
+ * - children: 각 페이지(Route) 내용
  */
 const Layout = ({ user, children }) => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
+      {/* 상단 네비게이션 */}
       <Navbar user={user} />
 
+      {/* 메인 콘텐츠 */}
       <main className="flex-1 w-full max-w-5xl mx-auto p-4 md:p-6">
         {children}
       </main>
@@ -19,4 +20,4 @@ const Layout = ({ user, children }) => {
   )
 }
 
-export default Layout
+export default Layout   // ✅ 반드시 있어야 App.jsx에서 import 가능
