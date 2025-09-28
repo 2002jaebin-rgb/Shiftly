1e69a76d66ab3b40c8720cadfb764e756923ab71
2002jaebin-rgb <2002jaebin@gmail.com>
2025-09-28 21:50:34 +0900

    UI_2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './tw.css'       // ✅ Tailwind (유틸리티 클래스)
+import './index.css'    // ✅ 기존 커스텀 CSS (btn, card 등 유지)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
